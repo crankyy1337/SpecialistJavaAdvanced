417d42ba1733dc9876c3cbb5cfef79d66011ec28
crankyy1337 <muradeligeorge@gmail.com>
2025-06-21 14:14:16 +0300

    first lab

---
+++ Lab1.BankAccount/src/lab1/bank/Main.java
@@ -0,0 +1,19 @@
+package lab1.bank;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankAccount b1 = new BankAccount("Source");
+        b1.deposit(1000);
+        BankAccount b2 = new BankAccount("Destination");
+        //печатаем перед трансфером
+        System.out.println(b1);
+        System.out.println(b2);
+        //трансфер и печать
+        System.out.println();
+        BankAccount.transfer(b1, b2, 300);
+        System.out.println(b1);
+        System.out.println(b2);
+
+    }
+}
